daa93e8d7f9e17b73cc90e5b9b8e289a0f4c2844
Stefan <stefan.von.stein@gmail.com>
2018-11-25 23:31:47 +0100

    project added

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject likely "0.1.0-SNAPSHOT"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/math.combinatorics "0.1.4"]
+                 [instaparse "1.4.9"]]
+  :java-source-paths ["java"] )
